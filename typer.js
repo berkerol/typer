26876491008376a98f6c500dc32d6b14fe2a43bd
Berk Erol <berk.erol@boun.edu.tr>
2017-10-11 04:34:21 +0300

    char explosion

--- typer.js
+++ typer.js
@@ -19,6 +19,12 @@ let char = {
   size: 30
 };
 
+let circle = {
+  decrease: 0.02,
+  opacity: 0.5,
+  total: 50
+};
+
 let label = {
   font: "16px Arial",
   color: "#0095DD",
@@ -29,6 +35,7 @@ let score = 0;
 let health = 10;
 
 let chars = [];
+let circles = [];
 
 draw();
 document.addEventListener("keydown", keyDownHandler);
@@ -37,6 +44,14 @@ function drawCenter() {
   ctx.beginPath();
   ctx.arc(center.x, center.y, center.radius, 0, 2 * Math.PI);
   ctx.fillStyle = center.color;
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawCircle(c) {
+  ctx.beginPath();
+  ctx.arc(c.x, c.y, c.radius, 0, 2 * Math.PI);
+  ctx.fillStyle = "rgba(" + c.r + ", " + c.g + ", " + c.b + ", " + circle.opacity + ")";
   ctx.fill();
   ctx.closePath();
 }
@@ -53,11 +68,25 @@ function drawLabel(message, number, position) {
   ctx.fillText(message + number, position, label.size);
 }
 
+function drawCircles() {
+  for (var j = circles.length - 1; j >= 0; j--) {
+    var c = circles[j];
+    drawCircle(c);
+    c.x += c.dx;
+    c.y += c.dy;
+    c.radius -= circle.decrease;
+    if (c.radius <= 0) {
+      circles.splice(j, 1);
+    }
+  }
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawCenter();
   drawLabel("Score: ", score, 10);
   drawLabel("Lives: ", health, canvas.width - 80);
+  drawCircles();
   createChars();
   removeChars();
   drawChars();
@@ -113,7 +142,25 @@ function die(start) {
     if (start) {
       alert("START AGAIN!");
       chars = [];
+      circles = [];
     }
+  }
+}
+
+function type(i, char) {
+  chars.splice(i, 1);
+  score++;
+  for (var j = 0; j < circle.total; j++) {
+    circles.push({
+      x: char.x,
+      y: char.y,
+      radius: 2 + Math.random() * 3,
+      dx: -5 + Math.random() * 10,
+      dy: -5 + Math.random() * 10,
+      r: Math.round(Math.random()) * 255,
+      g: Math.round(Math.random()) * 255,
+      b: Math.round(Math.random()) * 255
+    });
   }
 }
 
@@ -122,14 +169,12 @@ function keyDownHandler(e) {
     let char = chars[i];
     if (e.shiftKey) {
       if (e.keyCode === char.code) {
-        chars.splice(i, 1);
-        score++;
+        type(i, char);
         return;
       }
     } else {
       if (e.keyCode + 32 === char.code) {
-        chars.splice(i, 1);
-        score++;
+        type(i, char);
         return;
       }
     }
